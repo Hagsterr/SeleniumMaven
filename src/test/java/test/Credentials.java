c04b307e86da3d44dd752f9742e8931f4b4f2994
emilh <emilhagstrom01@gmail.com>
2023-04-18 13:30:46 +0200

    update 2.0

---
+++ src/test/java/test/Credentials.java
@@ -0,0 +1,35 @@
+package test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+class Credentials {
+
+    String email = null;
+    String password = null;
+
+        File jsonFile = new File("C:\\temp\\facebook.json");
+
+        {
+
+            try {
+
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode jsonNode = objectMapper.readTree(jsonFile);
+
+                email = jsonNode.get("facebookCredentials").get("email").asText();
+                password = jsonNode.get("facebookCredentials").get("password").asText();
+
+                System.out.println("Email: " + email);
+                System.out.println("Password: " + password);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
---
+++ src/test/java/test/facebookOpen.java
@@ -0,0 +1,31 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class facebookOpen {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\emilh\\Downloads\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        Credentials log = new Credentials();
+
+        // Get maximize Facebook.com
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com");
+
+
+        // Close cookies
+        driver.findElement(By.cssSelector("button[data-cookiebanner=\"accept_button\"]")).click();
+
+        // Log into facebook using credentials found in separate JSON file on computer for security
+        driver.findElement(By.id("email")).sendKeys(log.email);
+        driver.findElement(By.id("pass")).sendKeys(log.password);
+        driver.findElement(By.name("login")).click();
+
+
+
+    }
+}
