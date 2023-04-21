823898c11fab197152ce49412260adacb5cbc1ad
emilh <emilhagstrom01@gmail.com>
2023-04-21 11:04:35 +0200

    Separata packages

--- src/test/java/post/Credentials.java
+++ src/test/java/post/Credentials.java
@@ -1,4 +1,4 @@
-package test;
+package post;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
---
+++ src/test/java/search/facebookOpen.java
@@ -0,0 +1,56 @@
+package search;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class facebookOpen {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\testingGoogle\\chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        WebDriver driver = new ChromeDriver(options);
+        Credentials log = new Credentials();
+
+
+        // Get maximize Facebook.com
+        driver.get("https://www.facebook.com");
+        driver.manage().window().maximize();
+
+
+        // Close cookies
+        driver.findElement(By.cssSelector("button[data-cookiebanner=\"accept_button\"]")).click();
+
+        // Log into facebook using credentials found in separate JSON file on computer for security
+        driver.findElement(By.id("email")).sendKeys(log.email);
+        driver.findElement(By.id("pass")).sendKeys(log.password);
+        driver.findElement(By.name("login")).click();
+        driver.findElement(By.linkText("Startsida")).click();
+
+// Go to profile
+        driver.findElement(By.cssSelector("svg[aria-label='Your profile']")).click();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+// Click on logout button
+        driver.findElement(By.xpath("//span[text()='Log Out']")).click();
+
+// Sleep for 2 seconds
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
