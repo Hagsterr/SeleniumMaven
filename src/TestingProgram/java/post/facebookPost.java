b8e13c9665267cd5d222f8eb2e8214f06c724c71
Victor <victorastrom66@gmail.com>
2023-04-24 13:55:57 +0200

    Start

--- src/TestingProgram/java/post/facebookPost.java
+++ src/TestingProgram/java/post/facebookPost.java
@@ -29,17 +29,23 @@ private static final Logger logger = LoggerFactory.getLogger(facebookPost.class)
         driver.get("https://www.facebook.com");
         driver.manage().window().maximize();
 
+        //close cookies
 
-
-
-        // Close cookies
-        driver.findElement(By.cssSelector("button[data-cookiebanner=\"accept_button\"]")).click();
+        try {
+            driver.findElement(By.cssSelector("button[data-cookiebanner=\"accept_button\"]")).click();
+        } catch (Exception e) {
+            logger.error("Error occurred while accepting cookies: " + e.getMessage());
+        }
 
         // Log into facebook using credentials found in separate JSON file on computer for security
-        driver.findElement(By.id("email")).sendKeys(log.email);
-        driver.findElement(By.id("pass")).sendKeys(log.password);
-        driver.findElement(By.name("login")).click();
-        driver.findElement(By.linkText("Startsida")).click();
+        try {
+            driver.findElement(By.id("email")).sendKeys(log.email);
+            driver.findElement(By.id("pass")).sendKeys(log.password);
+            driver.findElement(By.name("login")).click();
+        } catch (Exception e) {
+            logger.error("Error occurred while logging into Facebook: " + e.getMessage());
+        }
+
 
         WebElement postBar = driver.findElement(By.cssSelector("#mount_0_0_Wq > div > div:nth-child(1) > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div > div > div.x78zum5.xdt5ytf.x10cihs4.x1t2pt76.x1n2onr6.x1ja2u2z > div.x9f619.x1n2onr6.x1ja2u2z.x78zum5.x2lah0s.xl56j7k.x1qjc9v5.xozqiw3.x1q0g3np.x1t2pt76.x17upfok > div > div.x9f619.x1n2onr6.x1ja2u2z.x78zum5.x1iyjqo2.xs83m0k.xeuugli.xl56j7k.x1qjc9v5.xozqiw3.x1q0g3np.x1iplk16.x1xfsgkm.xqmdsaz.x1mtsufr.x1w9j1nh > div > div > div > div.x78zum5.x1q0g3np.xl56j7k > div > div.x1yztbdb > div > div > div > div.x1cy8zhl.x78zum5.x1iyjqo2.xs83m0k.xh8yej3 > div"));
 
